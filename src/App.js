28652a415b2962e5aa31c76912b99a20b5979fdd
lazyshivam <shivamgoswami.ss.pp@gmail.com>
2024-01-22 00:32:49 +0530

    spinners added and lazy laoding implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import React, { useEffect, useState, lazy } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import BounceLoader from "react-spinners/BounceLoader";
 import "./App.css";
-import Container from "./components/Container";
-import Login from "./components/Login";
-import CreateUser from "./components/CreateUser";
-import { useEffect, useState } from "react";
-import Notification from "./components/Notification";
-import UserState from "./userContext/UserState";
-import VideoState from "./videoContext/VideoState";
-import ChatState from "./chatContext/ChatState";
+
+const Container = lazy(() => import("./components/Container"));
+const Login = lazy(() => import("./components/Login"));
+const CreateUser = lazy(() => import("./components/CreateUser"));
+const Notification = lazy(() => import("./components/Notification"));
+const UserState = lazy(() => import("./userContext/UserState"));
+const VideoState = lazy(() => import("./videoContext/VideoState"));
+const ChatState = lazy(() => import("./chatContext/ChatState"));
 
 function App() {
-
   const [token, setToken] = useState(localStorage.getItem("token"));
   const navigate = useNavigate();
 
@@ -19,31 +20,47 @@ function App() {
       navigate("/login");
     }
   }, [token]);
+
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    setToken(null); // Update the token state to trigger the navigation effect
+    localStorage.removeItem("token");
+    setToken(null);
   };
 
   return (
     <div
-      className="bg-slate-300 flex m-auto  justify-center   "
+      className="bg-slate-200 flex m-auto justify-center"
       style={{ height: "100vh" }}
     >
-      {!token ? (
+      <React.Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <BounceLoader className="text-center " color="#36d7b7" />
+          </div>
+        }
+      >
         <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/createUser" element={<CreateUser />} />
+          {!token ? (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/createUser" element={<CreateUser />} />
+            </>
+          ) : (
+            <Route
+              path="/"
+              element={
+                <UserState>
+                  <VideoState>
+                    <ChatState>
+                      <Notification />
+                      <Container onLogout={handleLogout} />
+                    </ChatState>
+                  </VideoState>
+                </UserState>
+              }
+            />
+          )}
         </Routes>
-      ) : (
-        <UserState>
-          <VideoState>
-            <ChatState>
-              <Notification />
-              <Container onLogout={handleLogout} />
-            </ChatState>
-          </VideoState>
-        </UserState>
-      )}
+      </React.Suspense>
     </div>
   );
 }
--- src/userContext/UserState.js
+++ src/userContext/UserState.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import UserContext from "./UserContext";
 import { url } from "../config";
+import BounceLoader from "react-spinners/BounceLoader";
 
 const UserState = (props) => {
   const [userProfile, setUserProfile] = useState(null);
   const [alluser, setAllUser] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -38,13 +40,13 @@ const UserState = (props) => {
 
           const userData = await userDataResponse.json();
           const allUsersData = await allUsersResponse.json();
-          // console.log("User Data:", userData);
-          // console.log("All Users Data:", allUsersData);
           setUserProfile(userData);
           setAllUser(allUsersData);
+          setLoading(false); // Set loading to false once data is fetched
         } catch (error) {
           // Handle error
           console.error("Error fetching data:", error);
+          setLoading(false); // Set loading to false in case of an error
         }
       };
 
@@ -52,9 +54,14 @@ const UserState = (props) => {
     }
   }, [isAuthenticated]);
 
+  if (loading) {
+    // Return loading spinner or any loading UI
+    return <div className="flex justify-center items-center"><BounceLoader color="#36d7b7"/></div>;
+  }
+
   return (
     <UserContext.Provider value={{ userProfile, alluser, isAuthenticated }}>
-      {isAuthenticated &&userProfile && props.children}
+      {isAuthenticated && userProfile && props.children}
     </UserContext.Provider>
   );
 };
